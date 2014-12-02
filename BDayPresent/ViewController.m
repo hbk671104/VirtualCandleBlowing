97cc2a30d8fa4d860f2b04369ebc3b6803cf5847
hbk671104 <hbk671104@gmail.com>
2014-12-01 20:33:42 -0500

    blow detection works

--- BDayPresent/ViewController.m
+++ BDayPresent/ViewController.m
@@ -7,16 +7,76 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
+#import <CoreAudio/CoreAudioTypes.h>
 
 @interface ViewController ()
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+	
+	AVAudioRecorder *recorder;
+	NSTimer *levelTimer;
+	double lowPassResults;
+	
+	BOOL blowTriggered;
+	
+}
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	
+	NSURL *url = [NSURL fileURLWithPath:@"/dev/null"];
+	
+	NSDictionary *settings = [NSDictionary dictionaryWithObjectsAndKeys:
+							  [NSNumber numberWithFloat: 44100.0],                 AVSampleRateKey,
+							  [NSNumber numberWithInt: kAudioFormatAppleLossless], AVFormatIDKey,
+							  [NSNumber numberWithInt: 1],                         AVNumberOfChannelsKey,
+							  [NSNumber numberWithInt: AVAudioQualityMax],         AVEncoderAudioQualityKey,
+							  nil];
+	NSError *error;
+	recorder = [[AVAudioRecorder alloc] initWithURL:url settings:settings error:&error];
+	
+	if (recorder) {
+		[recorder prepareToRecord];
+		[[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
+		[[AVAudioSession sharedInstance] setActive:YES error:nil];
+		recorder.meteringEnabled = YES;
+		[recorder record];
+		levelTimer = [NSTimer scheduledTimerWithTimeInterval:0.03
+													  target:self
+													selector:@selector(levelTimerCallback:)
+													userInfo:nil
+													 repeats:YES];
+	} else
+		NSLog(@"error %@",[error description]);
+	
+	// Haven't blowed yet
+	// 想到了邪恶的事情。。。。
+	blowTriggered = NO;
+	
+}
+
+- (void)levelTimerCallback:(NSTimer *)timer {
+	
+	[recorder updateMeters];
+	
+	const double ALPHA = 0.05;
+	double peakPowerForChannel = pow(10, (0.05 * [recorder peakPowerForChannel:0]));
+	lowPassResults = ALPHA * peakPowerForChannel + (1.0 - ALPHA) * lowPassResults;
+	
+	if (lowPassResults > 0.95 && !blowTriggered) {
+		UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Success"
+														message:@"Blow detected!"
+													   delegate:self
+											  cancelButtonTitle:@"Yes!"
+											  otherButtonTitles:nil, nil];
+		[alert show];
+		blowTriggered = YES;
+	}
+	
 }
 
 - (void)didReceiveMemoryWarning {
